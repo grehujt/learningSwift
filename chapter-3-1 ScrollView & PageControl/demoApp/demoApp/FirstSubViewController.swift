f3c0ca8554322d3118eacc629d9509ddfbf1554a
grehujt <kidd4212@gmail.com>
2016-08-03 00:13:32 +0800

    check in chapter 3-1

---
+++ chapter-3-1 ScrollView & PageControl/demoApp/demoApp/FirstSubViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FirstSubViewController.swift
+//  demoApp
+//
+//  Created by kris on 8/2/16.
+//  Copyright © 2016 kris. All rights reserved.
+//
+
+import UIKit
+
+class FirstSubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.brownColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ chapter-3-1 ScrollView & PageControl/readme.md
@@ -0,0 +1,31 @@
+# ScrollView & PageControl
+
+## key points
+- UIScrollView
+- UIPageControl
+- UIScreen.mainScreen().bounds
+- size.width
+- size.height
+- pagingEnabled
+- CGSizeMake
+- UIColor
+- contentSize
+- backgroundColor
+- delegate
+- CGRectMake
+- numberOfPages
+- currentPage
+- addTarget
+- origin.x
+- addSubview
+- origin.y
+- scrollRectToVisible
+- scrollViewDidScroll
+- frame.size.width
+- scrollViewWillBeginDecelerating
+
+## result
+
+![img](1.png)
+
+![img](2.png)
